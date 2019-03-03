1676b493de6b66e221f8fdc557e87351e0c941ec
Benjamin <swansonbenjamin434@yahoo.com>
2019-03-03 15:45:20 -0600

    Update Crag.lua

--- source/lua/Digest/Crag.lua
+++ source/lua/Digest/Crag.lua
@@ -1,13 +1,4 @@
--- ======= Copyright (c) 2003-2011, Unknown Worlds Entertainment, Inc. All rights reserved. =======
---
--- lua\Armory.lua
---
---    Created by:   Charlie Cleveland (charlie@unknownworlds.com)
---
--- ========= For more information, visit us at http://www.unknownworlds.com =====================
-
-
-
+Log("------------------Crag Loaded-------------------")
 function Crag:GetTechButtons(techId)
 
     return { kTechId.HealWave, kTechId.Move, kTechId.CragHeal, kTechId.None,
